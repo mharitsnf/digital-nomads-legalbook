c450746601d6d1a7375898916ce139e6ebf9678a
Lonerangerix <1137198072@qq.com>
2022-10-23 22:39:24 +0200

    ss

--- components/comment.js
+++ components/comment.js
@@ -1,21 +1,21 @@
 export default function Comment({name, time}) {
 
     return (
-        <div className="grid col-span-4 grid-cols-4 h-[8vh] w-[70%] justify-items-center content-center drop-shadow-lg self-center">
-           <div className="col-span-1">
+        <div className="grid col-span-4 grid-cols-4 w-[70%] justify-items-center content-center drop-shadow-lg self-center">
+           <div className="col-span-1 flex ">
             <img src={'/next.png'} alt="to" className="img-responsive w-full justify-center align-center" />
             </div> 
-            <div className="col-span-3 grid grid-rows-4">
+            <div className="col-span-3 grid grid-rows-3 w-full">
                 <div className="row-span-1 grid grid-col-3">
-                    <div className="col-span-1 h-full">
-                        <p className="text-[2vh] lg:text-lg ">{name}</p>
+                    <div className="col-span-1 h-full flex self-center content-center items-center">
+                        <p className="text-[2vh]">{name}</p>
                     </div>
-                    <div className="col-start-2 col-span-1 h-full ">
-                        <p className="text-[1vh] ">{time}</p>
+                    <div className="col-start-3 col-span-1 h-full flex justify-self-end h-full self-center content-center items-center">
+                        <p className="text-[1vh]">{time}</p>
                     </div>
                 </div>
-                <div className="row-span-3">
-                    <p className="text-[1vh] lg:text-lg align-center">It is really cool</p>
+                <div className="row-span-2">
+                    <p className="text-[1vh] align-center">It is really cool</p>
                 </div>
             </div>
         </div>
