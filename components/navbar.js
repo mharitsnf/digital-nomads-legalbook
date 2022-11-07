9faa84b0413d20c25714e52a0372a21fc8b7da47
Harits <mharitsnf@gmail.com>
2022-11-07 23:23:42 +0100

    Fix title

--- components/navbar.js
+++ components/navbar.js
@@ -6,7 +6,7 @@ export default function NavBar() {
         <div className="grid grid-cols-12 h-[10vh] w-full justify-items-center content-center shadow-lg">
             <div className="col-span-3 flex flex-row">
                 <img src='/bookLogo.png' alt="logo" className=" img-responsive h-[6vh] object-contain justify-center self-center" />
-                <Link href="/" className="pl-3 self-center font-semibold text-[2vh]">Digital Nomad Legalbook</Link>
+                <Link href="/" className=""><p className='pl-3 self-center font-semibold text-[2vh]'>Digital Nomad Legalbook</p></Link>
             </div>
             <div className="col-start-10 col-span-3">
                 <button className="bg-[#D9D9D9] py-[2vw] px-[2vh] lg:py-[1rem] lg:px-[2rem] rounded-lg text-[2vw] lg:text-lg">Language</button>
