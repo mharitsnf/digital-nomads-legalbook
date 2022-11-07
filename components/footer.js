48bf5310a556fed56a10378a72b86cf3ec4f6670
Lonerangerix <1137198072@qq.com>
2022-11-07 19:37:06 +0100

    logo added

--- components/footer.js
+++ components/footer.js
@@ -3,7 +3,11 @@ export default function Footer() {
     return (
         <div className="grid grid-cols-12 h-[10vh] w-full justify-items-center content-center drop-shadow-lg">
             <div className="col-span-2">
-                <p className="text-[2vh] lg:text-lg align-center">Logo</p>
+                <div className="flex flex-col self-center">
+                    <img src='/bookLogo.png' alt="logo" className="img-responsive h-[6vh] object-contain" />
+                    <p className="font-semibold">© 2022 Digital Nomad Legalbook</p>
+                </div>
+
             </div>
             <div className="col-start-10 col-span-3">
                 <button className="bg-[#D9D9D9] py-[2vw] px-[2vh] lg:py-[1rem] lg:px-[2rem] rounded-lg text-[2vw] lg:text-lg">Contact</button>
--- components/navbar.js
+++ components/navbar.js
@@ -1,11 +1,9 @@
-
-
 export default function NavBar() {
 
     return (
         <div className="grid grid-cols-12 h-[10vh] w-full justify-items-center content-center shadow-lg">
             <div className="col-span-2">
-                <p className="text-[2vh] lg:text-lg align-center">Logo</p>
+                <img src='/bookLogo.png' alt="logo" className=" img-responsive h-[6vh] object-contain justify-center align-center" />
             </div>
             <div className="col-start-10 col-span-3">
                 <button className="bg-[#D9D9D9] py-[2vw] px-[2vh] lg:py-[1rem] lg:px-[2rem] rounded-lg text-[2vw] lg:text-lg">Language</button>
